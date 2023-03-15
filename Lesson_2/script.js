2d37d9275858c11a2328b3a16f30033a8a28e065
Konstantin <t777tt88@gmai.com>
2023-03-15 20:37:57 +0100

    First Commit

---
+++ Lesson_2/script.js
@@ -0,0 +1,98 @@
+// Задана переменная number. Если значение в number больше или равно 10, то вывести в консоль 'hello', а если меньше - 'hi'
+
+// let number = 15;
+// number = 1;
+
+// if(number >= 10){
+//   console.log('hello')
+// } else {
+//   console.log('hi')
+// }
+
+// Тернарный оператор
+// number >= 10 ? console.log('hello'): console.log('hi');
+
+// Вывести в консоль все числа от 0 до 20
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i)
+// }
+
+// i = i + 1
+// i += 1
+// i++
+
+// i = i - 1
+// i -= 1
+// i--
+
+// 2. Вывести в консоль каждое третье число от 15 до 96
+// for (let i = 15; i <= 96; i += 3) {
+//     console.log(i)
+// }
+
+// 3. Вывести в консоль каждое третье число от 96 до 15
+
+// for (let i = 96; i >= 15; i -= 3) {
+//     console.log(i)
+// }
+
+
+
+// 1. Сформировать новый массив, в который попадут все положительные числа из массива numbers
+
+// const positive_numbers = [];
+// for(let i = 0; i < numbers.length; i++){
+//   if(numbers[i] > 0){
+//     positive_numbers.push(numbers[i])
+//   }
+// }
+
+// for(let i = 0; i < numbers.length; i++){
+//   numbers[i] > 0 ? positive_numbers.push(numbers[i]) : '';
+// }
+
+
+// const numbers = [6, 7, 1, -10, 78, 18, -5, -38, 9];
+// const multiply = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     multiply.push(numbers[i] * 2);
+// }
+
+// console.log(multiply);
+
+
+// Сформировать новый массив, в который попадут все положительные числа из массива numbers деленные на 2.
+
+// const numbers = [6, 7, 1, -10, 78, 18, -5, -38, 9];
+// const divided = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 0) {
+//     divided.push(numbers[i] / 2);
+//   }
+// }
+
+// console.log(divided);
+
+// const divSquare = document.createElement("div");
+// const a = 10;
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         const p = document.createElement("p")
+//         console.log(`Hello i am ${i} Even number`)
+//     } else { console.log(`Hello i am ${i} odd number`) }
+// }
+// let a = 10;
+// for (;;) {
+//     console.log (a)
+//   }
+
+// Написать цшкл while, который показывает строку сколько раз сколько вам 
+let i = 35;
+while (i < 35) {
+    console.log(`My age is ${i}`)
+}
+
